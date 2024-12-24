aedcfd34fdd6cdfd88d26ff6b32727c37215d48b
Yoshihito Kaneda <yoshihito.kaneda@gmail.com>
2024-12-24 17:52:38 +0900

    20241224 work

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,114 @@
 use rand::Rng;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Error;
+
+enum InningKind { Top, Bottom }
 const MAX_OUT: i32 = 3;
 
 fn main() {
-    let _avg :f64 = 0.3;
-    let mut out_count = 0;
+    let _avg :f64 = 0.45;
+    let mut inning_count = 0;
+    let mut inning_kind: InningKind  = InningKind::Bottom;
+    let mut is_in_game= true;
+    let mut top_innings: Vec<Inning> = Vec::new();
+    let mut bottom_innings: Vec<Inning> = Vec::new();
+
+    
     let mut _batting_first_scores: Vec<i32> = Vec::new();
     let mut _field_first_scores: Vec<i32> = Vec::new();
-    let mut is_first_runner: bool = false;
-    let mut is_second_runner: bool = false;
-    let mut is_third_runner: bool = false;
+    
+    let mut inning_seq = 0;
 
     let mut rng = rand::thread_rng();
+    _batting_first_scores.push(0);
+    _field_first_scores.push(0);
+    //let mut _current_scores: Vec<i32> = _batting_first_scores;
 
-    while out_count < MAX_OUT {
-        let trial: f64 = rng.gen();
-        println!("Trial: {trial}");
+    while is_in_game {
 
-        if _avg > trial {
-            println!("Hit!");
-
-            if is_second_runner {
-                is_third_runner = true;
-            }
-            if is_first_runner {
-                is_second_runner = true;
-            }
-            is_first_runner = true;
-            
+        if matches!(inning_kind, InningKind::Top) {
+            inning_kind = InningKind::Bottom;
         } else {
-            println!("Out!");
-            if out_count < MAX_OUT {
-                out_count += 1;
-            }
+            inning_kind = InningKind::Top;
+            inning_seq += 1;
         }
+
+        let mut inning = Inning { kind: InningKind::Top, seq: 1, counts: Vec::new(), score: 0};
+        let mut count_seq = 0;
+        let mut is_first_runner= false;
+        let mut is_second_runner= false;
+        let mut is_third_runner= false;
+        let mut out_count = 0;
+
+        while out_count < MAX_OUT {
+
+            count_seq += 1;
+            let mut count = Count {
+                 seq: count_seq,
+                is_first_runner: is_first_runner,
+                is_second_runner: is_second_runner,
+                is_third_runner: is_third_runner,
+                out: out_count,
+            };
+            let trial: f64 = rng.gen();
+            println!("Trial: {trial}");
     
-        println!("Score: 0-0");
-        println!("  <{}>", runner_text(is_second_runner));
-        println!("<{}> <{}>", runner_text(is_third_runner), runner_text(is_first_runner));
-        println!("  <H>");
-        println!("Out Count: {out_count}");
-    }
-
-
-
+            if _avg > trial {
+                println!("Hit!");
     
+                if is_third_runner {
+                    _batting_first_scores[0] = _batting_first_scores[0] + 1;
+                }
+                if is_second_runner {
+                    is_third_runner = true;
+                }
+                if is_first_runner {
+                    is_second_runner = true;
+                }
+                is_first_runner = true;
+                
+            } else {
+                println!("Out!");
+                if out_count < MAX_OUT {
+                    out_count += 1;
+                }
+            }
+        
+            println!("Score: {}-{}", _batting_first_scores[0], _field_first_scores[0]);
+            println!("  <{}>", runner_text(is_second_runner));
+            println!("<{}> <{}>", runner_text(is_third_runner), runner_text(is_first_runner));
+            println!("  <H>");
+            println!("Out Count: {out_count}");
+        }
 
-       
+
+
+
+
+
+
+
+        top_innings.push(inning);
+
+
+        if inning_count == 9 {
+            
+        }
+
+        is_in_game = false;
+        
+    } 
+           
+}
+
+fn process_inning(inning: Inning) -> Inning {
+
+    inning
+
+
+
+    //false
 }
 
 fn runner_text(runner: bool) -> &'static str {
@@ -54,4 +117,19 @@ fn runner_text(runner: bool) -> &'static str {
     } else {
         "-"
     }
+}
+
+struct Inning {
+    kind: InningKind,
+    seq: i32,
+    counts: Vec<Count>,
+    score: i32,
+}
+
+struct Count {
+    seq: i32,
+    is_first_runner: bool,
+    is_second_runner: bool,
+    is_third_runner: bool,
+    out: i32,
 }
